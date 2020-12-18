4241d3740ca6fcad2dfce2f6099741cb8998143b
sanditya <sanditya2002@gmail.com>
2020-12-19 05:17:42 +0700

    Fix Colorizer and Add Insertion Sort
    Add different print function for each sorting algorithm

--- main.c
+++ main.c
@@ -10,16 +10,17 @@ typedef struct color
    int bluePos;
 } Color;
 
-#define false -1
-
 void quickSort(int arr[], int high, int low, int size);
 void printBar(int value);
 void generateArray(int **arr, int size);
 void checkErrMalloc(int *ptr);
-void printArray(int *arr, int size, Color *Indices);
+void printBubbleSort(int *arr, int size, Color *Indices);
 void swap(int *n1, int *n2);
 void bubbleSort(int arr[], int size);
+void insertionSort(int arr[], int size);
+void printInsertionSort(int arr[], int size, Color *indices);
 void addColor(Color *Indices, int index);
+void delay(float numberOfSeconds);
 /*function prototype*/
 int main()
 {
@@ -31,7 +32,8 @@ int main()
    checkErrMalloc(array);
 
    generateArray(&array, size);
-   bubbleSort(array, size);
+   insertionSort(array, size);
+   // bubbleSort(array, size);
    // printArray(array, size);
 
    return 0;
@@ -78,46 +80,30 @@ void quickSort(int arr[], int high, int low, int size)
 
 void bubbleSort(int arr[], int size)
 {
+   system("cls");
    int i, j;
    Color indices;
    for (i = 0; i < size - 1; i++)
    {
       for (j = 0; j < size - 1 - i; j++)
       {
-         system("cls");
          indices.bluePos = j;
          indices.greenPos = size - i == 2 ? 0 : size - i;
          printf("\nindices.greenPos = %d, i = %d\n", indices.greenPos, i);
-         printArray(arr, size, &indices);
+         printBubbleSort(arr, size, &indices);
+         indices.redPos = -1;
+         system("cls");
          if (arr[j] > arr[j + 1])
          {
             swap(&arr[j], &arr[j + 1]);
+            indices.redPos = j;
          }
       }
    }
+   printBubbleSort(arr, size, &indices);
 }
 
-// void insertionSort(int arr[], int size)
-// {
-//    int i, j;
-//    int key;
-//    // printf("%d   %d\n", arr[low], arr[p]);
-
-//    for (i = 1; i < size; i++)
-//    {
-//       key = arr[i];
-//       j = i - 1;
-
-//       while (j >= 0 && arr[j] < key)
-//       {
-//          arr[j + 1] = arr[j];
-//          j = j - 1;
-//       }
-//       arr[j + 1] = key;
-//    }
-// }
-
-void printArray(int *arr, int size, Color *Indices)
+void printBubbleSort(int *arr, int size, Color *Indices)
 {
 
    int i; /*counter*/
@@ -133,14 +119,53 @@ void printArray(int *arr, int size, Color *Indices)
    printf("\033[0m");
    printf("\nPress Anything to Continue");
    getch();
+   // delay(((float)3) / size);
+}
+
+void insertionSort(int arr[], int size)
+{
+   int i, j;
+   int key;
+   // printf("%d   %d\n", arr[low], arr[p]);
+   Color indices;
+   for (i = 1; i < size; i++)
+   {
+      key = arr[i];
+      j = i - 1;
+      system("cls");
+      while (j >= 0 && arr[j] > key)
+      {
+
+         indices.redPos = i;
+         indices.bluePos = j;
+         printInsertionSort(arr, size, &indices);
+         system("cls");
+         arr[j + 1] = arr[j];
+         j = j - 1;
+      }
+      arr[j + 1] = key;
+      indices.redPos = i;
+      indices.bluePos = j;
+      printInsertionSort(arr, size, &indices);
+   }
+}
+
+void printInsertionSort(int arr[], int size, Color *indices)
+{
+   int i, j;
+   for (i = 0; i < size; i++)
+   {
+      addColor(indices, i);
+      printBar(arr[i]);
+      printf("\033[0m");
+   }
+   printf("\033[0m");
+   printf("\nPress Anything to Continue");
+   getch();
 }
 
 void addColor(Color *Indices, int index)
 {
-   // if (index == false)
-   // {
-   //    printf("\033[0m");
-   // }
    if (index == Indices->redPos)
    {
       printf("\033[1;31m");
@@ -181,4 +206,16 @@ void checkErrMalloc(int *ptr)
       printf("\nERROR! MEMORY CANNOT BE ALLOCATED");
       exit(0);
    }
+}
+void delay(float numberOfSeconds) //function to delay or take time
+{
+   // Converting time into milli_seconds
+   int milliSeconds = 1000 * numberOfSeconds;
+
+   // Storing start time
+   clock_t startTime = clock();
+
+   // looping till required time is not achieved
+   while (clock() < startTime + milliSeconds)
+      ;
 }
